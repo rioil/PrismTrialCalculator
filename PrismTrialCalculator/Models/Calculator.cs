51c0b18da2c789226f65aa98e0a1dc63a76717f3
rioil <rioil.dev@gmail.com>
2022-10-10 11:34:57 +0900

    簡易電卓機能を実装

---
+++ PrismTrialCalculator/Models/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismTrialCalculator.Models
+{
+    public class Calculator
+    {
+        public int Calculate(EOperator ope, int operand1, int operand2)
+        {
+            return ope switch
+            {
+                EOperator.Add => operand1 + operand2,
+                EOperator.Subtract => operand1 - operand2,
+                EOperator.Multiply => operand1 * operand2,
+                EOperator.Divide => operand1 / operand2,
+                _ => throw new ArgumentException("対応していない演算子です．", nameof(ope))
+            };
+        }
+    }
+
+    public enum EOperator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    }
+}
--- PrismTrialCalculator/ViewModels/MainWindowViewModel.cs
+++ PrismTrialCalculator/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,56 @@
-﻿using Prism.Mvvm;
+﻿using Livet.Messaging;
+using Prism.Mvvm;
+using PrismTrialCalculator.Models;
+using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace PrismTrialCalculator.ViewModels
 {
-    public class MainWindowViewModel : BindableBase
+    public class MainWindowViewModel : BindableBase, IDisposable
     {
-        private string _title = "Prism Application";
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+        private readonly CompositeDisposable _disposables = new();
+
+        public InteractionMessenger Messenger { get; } = new();
+
+        public ReactiveProperty<string> Title { get; set; } = new("Prism Application");
+
+        public ReactiveProperty<int> Operand1 { get; set; }
+        public ReactiveProperty<int> Operand2 { get; set; }
+        public ReactivePropertySlim<EOperator> Operator { get; set; } = new();
+        public ReactivePropertySlim<int> Result { get; set; } = new();
 
         public MainWindowViewModel()
         {
+            Operand1 = new ReactiveProperty<int>()
+                .SetValidateNotifyError(value => value > 100 ? "100以下の数値を入力してください．" : null);
+            Operand2 = new ReactiveProperty<int>()
+                .SetValidateNotifyError(value => value < 0 ? "0以上の数値を入力してください．" : null);
 
+            CaluculateCommand = Observable.CombineLatest(Operand1.ObserveHasErrors, Operand2.ObserveHasErrors)
+               .Select(x => x.All(x => !x))
+               .ToReactiveCommand()
+               .WithSubscribe(() =>
+               {
+                   var calculator = new Calculator();
+                   try
+                   {
+                       Result.Value = calculator.Calculate(Operator.Value, Operand1.Value, Operand2.Value);
+                   }
+                   catch (DivideByZeroException ex)
+                   {
+                       Messenger.Raise(new InformationMessage(ex.Message, "Errror", "Information"));
+                   }
+               });
+        }
+
+        public ReactiveCommand CaluculateCommand { get; set; }
+
+        public void Dispose()
+        {
+            _disposables.Dispose();
         }
     }
 }
